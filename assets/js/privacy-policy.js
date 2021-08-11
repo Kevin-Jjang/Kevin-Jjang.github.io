32acd582f513841b9b3aaff2dc540a1ec37cdbd5
Kevin-Jjang <k1jang@ucsd.edu>
2021-08-11 00:42:00 -0700

    Privacy Policy and ToS

---
+++ assets/js/privacy-policy.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let hidePrivacyPolicy = window.localStorage.getItem('hidePrivacyPolicy081021');
+    if(hidePrivacyPolicy === 'true' || document.getElementById('privacy-policy')) {
+        closePrivacyPopup();
+    }
+});
+
+function hidePrivacyPopup() {
+    window.localStorage.setItem('hidePrivacyPolicy081021', true);
+    let privacyPopup = document.getElementById('privacy-policy-popup');
+    privacyPopup.classList.remove('visible');
+    privacyPopup.classList.add('hidden');
+}
+
+function closePrivacyPopup() {
+    let privacyPopup = document.getElementById('privacy-policy-popup');
+    privacyPopup.classList.remove('visible');
+    privacyPopup.classList.add('hidden');
+}
