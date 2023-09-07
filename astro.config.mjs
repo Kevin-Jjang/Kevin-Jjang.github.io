27bf1f04bad1c23c2f434bab1c4e0ed1dfb6f471
Kevin-Jjang <k1jang@ucsd.edu>
2023-09-07 16:48:27 -0700

    Changed site name to github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'kevinjjang.me',
+  site: 'Kevin-Jjang.github.io',
   base: '/'
 });
